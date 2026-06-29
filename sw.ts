612ced4c33e4fd253e5280f3b0c7bcd653f995d2
Alex <alexandervilla.cuenta@gmail.com>
2026-06-29 17:32:54 -0300

    fix: replace ServiceWorkerGlobalScope with Window type to fix TS build error

--- sw.ts
+++ sw.ts
@@ -1,9 +1,10 @@
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "serwist";
 
-declare const self: ServiceWorkerGlobalScope & {
-  __SW_MANIFEST: (string | { url: string; revision: string | null })[];
-};
+declare const self: Window &
+  typeof globalThis & {
+    __SW_MANIFEST: (string | { url: string; revision: string | null })[];
+  };
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
